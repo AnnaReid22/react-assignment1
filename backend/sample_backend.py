209510548d44cd2a625d71883b96f2926c197556
Anna Reid <41653027+AnnaReid22@users.noreply.github.com>
2021-09-27 13:25:22 -0700

    step 3: adding a search by id

--- backend/sample_backend.py
+++ backend/sample_backend.py
@@ -33,6 +33,7 @@ users = {
    ]
 }
 
+@app.route('/users')
 def get_users():
    search_username = request.args.get('name') #accessing the value of parameter 'name'
    if search_username :
@@ -41,6 +42,15 @@ def get_users():
          if user['name'] == search_username:
             subdict['users_list'].append(user)
       return subdict
+   return users
+
+@app.route('/users/<id>')
+def get_user(id):
+   if id :
+      for user in users['users_list']:
+        if user['id'] == id:
+           return user
+      return ({})
    return users
    
 @app.route('/')
